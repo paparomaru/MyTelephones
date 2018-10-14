11ada19e5e5620d8cf6f6a858d740bdea8dd4be0
vinokurov63@gmail.com <vinokurov63@gmail.com>
2018-10-14 11:34:13 +0300

    Работает почти все. Не создается модель.

--- MyTele/forms.py
+++ MyTele/forms.py
@@ -6,7 +6,7 @@ class AddressBookEntryForm(forms.Form):
     name = forms.CharField(max_length=100)
     surname = forms.CharField(max_length=100)
     telephone = forms.RegexField(
-        regex=r'^\+[0-9]{2}\s[0-9]+\s[0-9]{6,}$',
+        regex=r'^\+[0-9]{2}\s[0-9]+\s[0-9]{5,}$',
         max_length=20,
         error_messages={
             'invalid': u'Telephone number specified is not correct. It must be of the form: +XX XXXX XXXXXX'
