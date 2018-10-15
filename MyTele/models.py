8befe67a9e55f23d85ebc8a62a8d242feda2e0c8
vinokurov63@gmail.com <vinokurov63@gmail.com>
2018-10-16 00:07:53 +0300

    Рабочая переведена на русский.

--- MyTele/models.py
+++ MyTele/models.py
@@ -10,4 +10,3 @@ class AddressBookEntry(models.Model):
         return u'{0} {1} ({2})'.format(self.name,
                                        self.surname,
                                        self.telephone)
-# комменты
