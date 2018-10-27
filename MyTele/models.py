31e9fd22524ee3fa3d6ef29ef6e3b4e341b3c9d1
vinokurov63@gmail.com <vinokurov63@gmail.com>
2018-10-27 19:32:05 +0300

    + add model addressbook fields email, komment

--- MyTele/models.py
+++ MyTele/models.py
@@ -5,8 +5,11 @@ class AddressBookEntry(models.Model):
     name = models.CharField(max_length=100)
     surname = models.CharField(max_length=100)
     telephone = models.CharField(max_length=20)
+    email = models.EmailField(max_length=255, default='example@mail.ru')
+    komment = models.TextField(default='Komment')
 
     def __str__(self):
-        return u'{0} {1} ({2})'.format(self.name,
-                                       self.surname,
-                                       self.telephone)
+        return u'{0} {1} ({2}) {3}'.format(self.name,
+                                           self.surname,
+                                           self.telephone,
+                                           self.email)
--- MyTelephones/settings.py
+++ MyTelephones/settings.py
@@ -13,8 +13,9 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+from django.conf.global_settings import STATICFILES_DIRS
 
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
@@ -26,7 +27,6 @@ SECRET_KEY = 'b&q_x7q4*an6sh)a(_gs929#e8+adfb!_-i8%6-0yfl)$vjqvb'
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -71,7 +71,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'MyTelephones.wsgi.application'
-
 
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
@@ -152,8 +151,8 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
