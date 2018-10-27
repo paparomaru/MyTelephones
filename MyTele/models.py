f9108c4b09a3b156c71d6b31b069cbf9a18dbf9b
vinokurov63@gmail.com <vinokurov63@gmail.com>
2018-10-27 20:08:50 +0300

    + добавлены два поля: created_at, modified_at

--- MyTele/models.py
+++ MyTele/models.py
@@ -2,6 +2,8 @@ from django.db import models
 
 
 class AddressBookEntry(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True, null=True)
+    modified_at = models.DateTimeField(auto_now=True, null=True)
     name = models.CharField(max_length=100)
     surname = models.CharField(max_length=100)
     telephone = models.CharField(max_length=20)
@@ -9,7 +11,8 @@ class AddressBookEntry(models.Model):
     komment = models.TextField(default='Komment')
 
     def __str__(self):
-        return u'{0} {1} ({2}) {3}'.format(self.name,
-                                           self.surname,
-                                           self.telephone,
-                                           self.email)
+        return u' name:{0} surname:{1} tel:{2}) email:{3} komment:{4}'.format(self.name,
+                                                                              self.surname,
+                                                                              self.telephone,
+                                                                              self.email,
+                                                                              self.komment[0:50])
